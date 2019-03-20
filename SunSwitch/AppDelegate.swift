8e0c8f0587faec7210c2b0e4f93f8d6ae5dd7bbf
Jack Wiig <jhwiig@gmail.com>
2019-03-20 05:29:00 -0500

    Initial Commit

---
+++ SunSwitch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SunSwitch
+//
+//  Created by Jack Wiig on 3/20/19.
+//  Copyright © 2019 Jack Wiig. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
